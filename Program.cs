6505ce659fea62ea1aaa7bdb7deca14ce0f9821d
Etomidate <68708079+Etomidate@users.noreply.github.com>
2022-08-27 15:34:57 -0700

    added-code
    added game code and beginning debug

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿/* logic and conditional statements to write a classic text-based 
+Choose Your Own Adventure Game. 
+*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //variables
+        string userName;
+        string noiseChoice;
+        string doorChoice;
+        string riddleAnswer = " ";
+        string keyChoice = " ";
+        /* the mysterious noise */
+
+
+            //ask the user's name
+            Console.Write("WHAT IS YOUR NAME?: ");
+            userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}! Welcome to our story!");
+
+            Console.WriteLine("It begins on a cold rainy night. \n" +
+            "You're sitting in your room and hear a noise coming \n" +
+            "from down the hall. \n DO YOU GO INVESTIGATE?");
+
+            Console.Write("Type YES or NO \n ");
+            noiseChoice = Console.ReadLine();
+            noiseChoice = noiseChoice.ToUpper();
+
+            if(noiseChoice == "NO")
+            {
+                Console.WriteLine("Not much of an adventure if we don't \n" +
+                "leave our room! \n THE END");
+            }
+            else if(noiseChoice == "YES");
+            {
+                Console.WriteLine("You walk into the hallway and see a \n" + 
+                "light coming from under a door down the hall. You walk \n" +
+                "towards it. \n DO YOU OPEN IT OR KNOCK?");
+            }
+
+            Console.Write("Type OPEN or KNOCK");
+            doorChoice = Console.ReadLine();
+            doorChoice = doorChoice.ToUpper();
+
+            if( doorChoice == "KNOCK")
+            {
+                Console.WriteLine("A voice behind the door speaks. \n"
+                                + "It says, \"Answer this riddle: Poor people have it\""
+                                + "\"Rich people need it. If you eat it you die.\""
+                                + "\n"
+                                + "What is it?");
+                Console.Write("Type your answer:");
+                riddleAnswer = riddleAnswer.ToUpper();
+                    if(riddleAnswer == "NOTHING")
+                    {
+                        Console.WriteLine("The door opens and NOTHING is"+
+                        "there. You turn off \n the light and run back to"+
+                        "your room and lock the door. \n THE END");
+                    }
+                    else
+                    {
+                    Console.WriteLine("You answered incorrectly. \n"+
+                    "The door doesn't open. \n THE END"); 
+                    }
+            }
+            else if(doorChoice == "OPEN")
+            {
+                Console.WriteLine("The door is locked! \n See if one of your"+
+                "three keys will open it.");
+                Console.Write("Your have three keys numbered 1-3 \n"+
+                "Type the numbe of the key you would like to use");
+                keyChoice = keyChoice.ToUpper();
+                switch(keyChoice)
+                {
+                    case "1":
+                    Console.WriteLine("You choose the first key. \n"+
+                        "Lucky choice! \n The door opens and NOTHING is there. \n"+
+                        "Strange... \n THE END");
+                        break;
+                    case "2":
+                        Console.WriteLine("You choose the second key. \n The door"+
+                        "doesn't open. \n THE END");
+                        break;
+                    case "3":
+                        Console.WriteLine("You choose the third key. \n The door"+
+                        "doesn't open. \n THE END");
+                        break;
+                    default:
+                        Console.WriteLine("You only have keys 1-3! Type the number of the key!");
+                    break;
+                }
+            }
+}
+}
